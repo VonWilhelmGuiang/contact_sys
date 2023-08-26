a1db97f257f04e5475844c0925fe1b15f9fb1ae1
Von Wilhelm Guiang <guiang.vw@gmail.com>
2023-08-26 10:06:38 +0800

    added create user

--- user/ajax.py
+++ user/ajax.py
@@ -18,6 +18,7 @@ def authenticate_user(request):
             if user:
                 # check password
                 if(check_password(form.cleaned_data['password'],user['password'])):
+                    request.session['user_id'] = user[id]
                     return JsonResponse({'success':True}, status=200)
                 else:
                     return JsonResponse({ 'success':False, 'message': 'Incorrect password' }, status=403)    
@@ -33,7 +34,26 @@ def authenticate_user(request):
 def create_user(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
+
+        # validate form
         if form.is_valid():
-            return JsonResponse({'test':'123123123'}, status=200)
+            # validate password
+            if form.cleaned_data['password'] == form.cleaned_data['confirm_password']:
+                insert = Account.objects.create(
+                    username = form.cleaned_data['username'],
+                    email = form.cleaned_data['email'],
+                    first_name = form.cleaned_data['first_name'],
+                    last_name = form.cleaned_data['last_name'],
+                    password = make_password(form.cleaned_data['password'])
+                )
+                if insert:
+                    return JsonResponse({'message': 'User Created'}, status=201)
+                else:
+                    return JsonResponse({'message': 'An error has occured'}, status=400)
+            else:
+                return JsonResponse({'message':'Password do not match'}, status= 403)
         else:
-            return JsonResponse({'message': form.errors.as_json()}, status=400)
+            if form.errors.as_json():
+                return JsonResponse({'message': form.errors.as_json()}, status=400)
+            else:
+                return JsonResponse({'message': 'Invalid Request'},status=400)
